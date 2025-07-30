066ad2bebaa886af74e8b1d6019c2868d88b3c47
vibulan73 <vibulan2000@gmail.com>
2025-07-30 11:14:23 +0530

    URL Params

---
+++ src/main/java/com/jv/HelloWorld/TodoController.java
@@ -0,0 +1,43 @@
+package com.jv.HelloWorld;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/todo")
+public class TodoController {
+    @GetMapping("/get")
+    String getTodo(){
+        return "Todo";
+    }
+
+    //Path Variable
+    @GetMapping("/{id}")
+    String getTodoById(@PathVariable long id ){
+        return "Todo with ID " + id;
+    }
+
+    //Request Param
+    @GetMapping
+    String getTodoByIdParam(@RequestParam("todoId") long id ){
+        return "Todo with ID " + id;
+    }
+//    wrong as password is visible to all users
+//    @GetMapping("/create")
+//    String createUser(@RequestParam String userId, @RequestParam String password ){
+//        return "Todo with ID " + userId + " " + "password: " + password;
+//    }
+    @PostMapping("/create")
+    String createUser(@RequestBody String body){
+        return body;
+    }
+
+    @PutMapping("/{id}")
+    String updateTodoById(@PathVariable long id){
+        return "Update Todo with ID " + id;
+    }
+
+    @DeleteMapping("/{id}")
+    String deleteTodoById(@PathVariable long id){
+        return "Delete Todo with ID " + id;
+    }
+}
